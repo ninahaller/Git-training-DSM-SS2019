fe519b55cfe45c5ef1751ea23604ecf4a446429a
Alfred Franz <franz@hs-ulm.de>
2019-03-19 21:28:55 +0100

    initial commit

---
+++ src/helloWorld.java
@@ -0,0 +1,19 @@
+public class helloWorld{
+	public static void main(String[] args){
+			System.out.println("Hello Team 1!");
+			System.out.println("Hello Fi.S.!");
+			System.out.println("Hello Sü.A.!");
+			System.out.println("Hello Jo.F.!");
+			System.out.println("Hello Se.S.!");
+			System.out.println("Hello Team 2!");
+			System.out.println("Hello De.E.!");
+			System.out.println("Hello Ti.F.!");
+			System.out.println("Hello Ti.L.!");
+			System.out.println("Hello Fa.W.!");
+			System.out.println("Hello Team 3!");
+			System.out.println("Hello Al.N.M.!");
+			System.out.println("Hello Sa.S.!");
+			System.out.println("Hello Ph.S.!");
+			System.out.println("Hello Ni.H.!");		
+	}
+}
