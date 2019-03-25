145407dc40c461619e9a6bc5fe9d22131f576336
Git-Training <timolahres@hotmail.com>
2019-03-25 17:36:29 +0100

    changed syntax / deleted unknown code

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -8,14 +8,11 @@ public class helloWorld{
 			System.out.println("Hello Team 2!");
 			System.out.println("Hello De.E.!");
 			System.out.println("Hello Ti.F.!");
-<<<<<<< HEAD
 			System.out.println("Hello Ti.L.!");
 			System.out.println("Hello Anderst.W.!");
 			System.out.println("Etwas geaendert in Eclipse");
-=======
 			System.out.println("Hallo mein Name ist Timo Lahres!");
 			System.out.println("Hello Fa.W.!");
->>>>>>> Timo
 			System.out.println("Hello Team 3!");
 			System.out.println("Hello Al.N.M.!");
 			System.out.println("Hello Sa.S.!");
