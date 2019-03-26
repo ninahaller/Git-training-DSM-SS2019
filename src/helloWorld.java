c5ce58070031d400227366a014917aa86f285237
HS-ULM\tfiedler <tfiedler@mail.hs-ulm.de>
2019-03-26 14:57:23 +0100

    Name geändert

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -6,8 +6,8 @@
 			System.out.println("Hello Jo.F.!");
 			System.out.println("Hello Se.S.!");
 			System.out.println("Hello Team 2!");
-			System.out.println("Hello De.E. verändert und name veraendert!");
-			System.out.println("Hello Ti.F.!");
+			System.out.println("Hello De.E. verï¿½ndert und name veraendert!");
+			System.out.println("Hello Tizia Fiedler!");
 			System.out.println("Hello Ti.L.!");
 			System.out.println("Hello Anderst.W.!");
 			System.out.println("Etwas geaendert in Eclipse");
