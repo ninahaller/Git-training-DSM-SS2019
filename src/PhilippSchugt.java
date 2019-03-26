21c74721df6838ddd6593fd458014962c70b1e94
Alfred Franz <franz@hs-ulm.de>
2019-03-26 16:26:58 +0100

    Fixed unsolved merge conflicts

--- src/PhilippSchugt.java
+++ src/PhilippSchugt.java
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-package src;
-=======
->>>>>>> fehlerfrei
+
 
 public class PhilippSchugt {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-=======
+
 	System.out.print("Hallo Klasse");
->>>>>>> fehlerfrei
 
 	}
 
