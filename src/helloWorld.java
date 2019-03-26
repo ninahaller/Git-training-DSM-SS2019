d57df896559e5108968ac811fc34d0a7140c6f63
k_alime <alime.ka@web.de>
2019-03-26 15:21:20 +0100

    Änderung

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -6,14 +6,14 @@
 			System.out.println("Hello Jo.F.!");
 			System.out.println("Hello Se.S.!");
 			System.out.println("Hello Team 2!");
-			System.out.println("Hello De.E. verändert und name veraendert!");
+			System.out.println("Hello De.E. verï¿½ndert und name veraendert!");
 			System.out.println("Hello Ti.F.!");
 			System.out.println("Hello Ti.L.!");
 			System.out.println("Hello Anderst.W.!");
 			System.out.println("Etwas geaendert in Eclipse");
 			System.out.println("Hallo mein Name ist Timo Lahres!");
 			System.out.println("Hello Team 3!");
-			System.out.println("Hello Al.N.M.!");
+			System.out.println("Hello Al.N.M.! Ã„nderung");
 			System.out.println("Hello Sa.S.!");
 			System.out.println("Hello Ph.S.! Hello");
 			System.out.println("Hello Ni.H.!");		
